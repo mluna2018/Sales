f4ae708809fe52f31c3c9ddf8d4d4b302a8764b6
x240 <x240@LENOVO>
2018-11-07 23:06:27 -0500

    DataConnection

---
+++ Sales.Common/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Sales.Common.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        public String Description { get; set; }
+
+        public decimal price { get; set; }
+
+        public bool Isavailable { get; set; }
+
+        public DateTime PublishOn { get; set; }
+    }
+}
---
+++ Sales.Domain/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace Sales.Domain.Models
+{
+    using System.Data.Entity;
+
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+
+        }
+    }
+}
