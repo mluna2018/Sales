cd5f47f001e9c5789e9ea06cc12cc6e83b0e5341
mauricio.luna <mauricio.luna@ANTELE07M>
2018-11-21 21:39:06 -0500

    ViewModels

--- Sales/Sales/ViewModels/ProductsViewModel.cs
+++ Sales/Sales/ViewModels/ProductsViewModel.cs
@@ -2,10 +2,44 @@
 
 namespace Sales.ViewModels
 {
-    using Sales.Common.Models;
+    using Common.Models;
+    using Services;
+    using System;
+    using System.Collections.Generic;
     using System.Collections.ObjectModel;
-    public class ProductsViewModel
+    using Xamarin.Forms;
+
+    public class ProductsViewModel : BaseViewModel
     {
-        public ObservableCollection<Product> Products { get; set; }
+        private ApiService apiService;
+
+        private ObservableCollection<Product> products;
+
+        public ObservableCollection<Product> Products
+        {
+            get { return this.products; }
+            set {this.SetValue(ref this.products, value); }
+        }
+
+        public ProductsViewModel()
+        {
+            this.apiService = new ApiService();
+            this.LoadProducts();
+            
+        }
+
+        private async void LoadProducts()
+        {
+            var response = await this.apiService.GetList<Product>("https://salesapitecnonline.azurewebsites.net", "/api", "/Products");
+            if(!response.IsSuccess)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", response.Message, "Accept");
+                return;
+            }
+
+            var list = (List<Product>)response.Result;
+            this.Products = new ObservableCollection<Product>(list);
+        }
+        
     }
 }
